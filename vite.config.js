611add2977305e8030e12ebc51eee20b9a526d0f
aya mahmoud <aya122972@gmail.com>
2025-09-09 02:22:17 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // base:'./' github دا لو مش هربط بال 
+  //github دا لما اربط بال 
+  base:'/vite_repo'
+})
